53e30251a653be4da0bbbfcc60a340ca0af66432
Aswaj <aswajcheruvath@gmail.com>
2024-01-04 20:39:05 +0530

    profilepage

---
+++ lib/profile_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  const ProfilePage({super.key});
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor:  Color.fromARGB(255, 167, 163, 152),
+      
+      appBar: AppBar(backgroundColor:const  Color.fromARGB(255, 188, 170, 90),
+      title:  Text('PROFILE PAGE',
+      ),
+      ),
+      
+     //body
+
+
+     
+    );
+  }
+}
+
